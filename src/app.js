b2e517030701fd884439ad97fb6b4177c1f301cb
HAlbergson <hjalte3480@gmail.com>
2023-11-22 13:39:54 +0100

    get virker kinda

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { config } from "dotenv";
 import { Sequelize, DataTypes } from "sequelize";
+
+config();
 
 const app = express();
 const port = 3000; // Du kan ændre dette til den ønskede port
@@ -10,33 +13,46 @@ console.log(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQ
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
   host: process.env.MYSQL_HOST,
   dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+  insecureAuth: true,
+  logging: console.log, // Aktiver debugging
 });
 
 // Definer en model
-const Author = sequelize.define("Author", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
+const Author = sequelize.define(
+  "Author",
+  {
+    author_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birth_year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    biography: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
   },
-  image: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  birth_year: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  biography: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false, // Dette vil deaktivere createdAt og updatedAt
+  }
+);
 
 // Synkronisér modellen med databasen (opretter tabellen)
 Author.sync()
@@ -50,7 +66,9 @@ Author.sync()
 // Opret en GET-rute for at hente alle forfattere
 app.get("/authors", async (req, res) => {
   try {
+    console.log("GET-ruten blev ramt");
     const authors = await Author.findAll();
+    console.log("Data fra databasen:", authors);
     res.json(authors);
   } catch (error) {
     console.error("Fejl ved hentning af forfattere:", error);
