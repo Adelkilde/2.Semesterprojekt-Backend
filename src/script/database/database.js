9e8e2ea5db0cf7f9a51014341258db0f5f8ddad9
LauritsSchou <laurits.schou@gmail.com>
2023-11-27 11:48:14 +0100

    Sequelize working as intended

--- src/script/database/database.js
+++ src/script/database/database.js
@@ -1,4 +1,3 @@
-import sequelize from "sequelize";
 import { Sequelize, DataTypes } from "sequelize";
 import { config } from "dotenv";
 
@@ -21,152 +20,185 @@ const sequelize = new Sequelize(
   }
 );
 
-const Author = sequelize.define("Author", {
-  author_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Author = sequelize.define(
+  "Author",
+  {
+    author_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    date_of_birth: {
+      type: DataTypes.DATE,
+    },
+    biography: {
+      type: DataTypes.TEXT,
+    },
   },
-  image: {
-    type: DataTypes.STRING,
-  },
-  name: {
-    type: DataTypes.STRING,
-  },
-  birth_year: {
-    type: DataTypes.INTEGER,
-  },
-  biography: {
-    type: DataTypes.TEXT,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-const Work = sequelize.define("Work", {
-  work_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Work = sequelize.define(
+  "Work",
+  {
+    work_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    title: {
+      type: DataTypes.STRING,
+    },
+    publication_date: {
+      type: DataTypes.DATE,
+    },
+    publisher: {
+      type: DataTypes.STRING,
+    },
+    average_rating: {
+      type: DataTypes.DECIMAL(3, 2),
+    },
+    purchase_link: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
   },
-  image: {
-    type: DataTypes.STRING,
-  },
-  title: {
-    type: DataTypes.STRING,
-  },
-  publication_date: {
-    type: DataTypes.DATE,
-  },
-  publisher: {
-    type: DataTypes.STRING,
-  },
-  average_rating: {
-    type: DataTypes.DECIMAL(3, 2),
-  },
-  purchase_link: {
-    type: DataTypes.STRING,
-  },
-  description: {
-    type: DataTypes.TEXT,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 Work.belongsTo(Author, { foreignKey: "author_id" });
 
-const Review = sequelize.define("Review", {
-  review_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Review = sequelize.define(
+  "Review",
+  {
+    review_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    review_text: {
+      type: DataTypes.TEXT,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-  },
-  email: {
-    type: DataTypes.STRING,
-  },
-  review_text: {
-    type: DataTypes.TEXT,
-  },
-  rating: {
-    type: DataTypes.INTEGER,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 Review.belongsTo(Work, { foreignKey: "work_id" });
 
-const News = sequelize.define("News", {
-  news_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const News = sequelize.define(
+  "News",
+  {
+    news_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    headline: {
+      type: DataTypes.STRING,
+    },
+    content: {
+      type: DataTypes.TEXT,
+    },
+    created_date: {
+      type: DataTypes.DATE,
+    },
   },
-  headline: {
-    type: DataTypes.STRING,
-  },
-  content: {
-    type: DataTypes.TEXT,
-  },
-  created_date: {
-    type: DataTypes.DATE,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 News.belongsTo(Author, { foreignKey: "author_id" });
 
-const ContactMe = sequelize.define("ContactMe", {
-  contact_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const ContactMe = sequelize.define(
+  "ContactMe",
+  {
+    contact_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    message: {
+      type: DataTypes.TEXT,
+    },
+    submitted_date: {
+      type: DataTypes.DATE,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-  },
-  email: {
-    type: DataTypes.STRING,
-  },
-  message: {
-    type: DataTypes.TEXT,
-  },
-  submitted_date: {
-    type: DataTypes.DATE,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 ContactMe.belongsTo(Author, { foreignKey: "author_id" });
 
-const SocialMedia = sequelize.define("SocialMedia", {
-  soMe_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const SocialMedia = sequelize.define(
+  "SocialMedia",
+  {
+    soMe_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    platform: {
+      type: DataTypes.STRING,
+    },
+    link: {
+      type: DataTypes.STRING,
+    },
   },
-  platform: {
-    type: DataTypes.STRING,
-  },
-  link: {
-    type: DataTypes.STRING,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 SocialMedia.belongsTo(Author, { foreignKey: "author_id" });
 
-sequelize.sync({ force: true }).then(async () => {
-  await Author.bulkCreate([
-    {
-      image: "path/to/image1.jpg",
-      name: "Author 1",
-      birth_year: 1980,
-      biography: "Biography for author 1",
-    },
-    {
-      image: "path/to/image2.jpg",
-      name: "Author 2",
-      birth_year: 1990,
-      biography: "Biography for author 2",
-    },
-  ]);
-
-  console.log("Database synchronized.");
-});
+Author.sync({ alter: true })
+  .then((data) => {
+    const author = Author.build({
+      image: "Test",
+      name: "test",
+      date_of_birth: "1995-08-06",
+      biography: "test test test test test",
+    });
+    console.log("Table and model created");
+    return author.save();
+  })
+  .then((data) => {})
+  .catch((err) => {
+    console.log("Error syncing table and model");
+  });
 
 export { sequelize, DataTypes };
