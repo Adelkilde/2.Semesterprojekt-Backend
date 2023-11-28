7b7d8d06ed7b5ffcf63f9bcdf6cc6e481c0073e7
Adelkilde <William.adelkilde@gmail.com>
2023-11-28 20:01:37 +0100

    Update authorRoutes.js

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -3,22 +3,32 @@ import Author from "../models/authorModel.js";
 
 // Function to handle requests to get all authors
 async function getAllAuthors(req, res) {
-  // Use the findAll method on the Author model to get all authors from the database
-  const authors = await Author.findAll();
-  // Send the authors as a JSON response
-  res.json(authors);
+  try {
+    // Use the findAll method on the Author model to find all authors
+    const authors = await Author.findAll();
+    // Send the authors as a JSON response
+    res.json(authors);
+  } catch (error) {
+    // If there is an error, send the error as a JSON response
+    res.json({ error: error.message });
+  }
 }
 
 // Function to handle requests to get an author by ID
 async function getAuthorById(req, res) {
-  // Use the findByPk method on the Author model to get an author by its primary key (ID)
-  const author = await Author.findByPk(req.params.id);
-  if (author) {
-    // If the author is found, send it as a JSON response
-    res.json(author);
-  } else {
-    // If the author is not found, send a 404 status code and a message
-    res.status(404).send("Author not found");
+  try {
+    // Use the findByPk method on the Author model to find an author by its primary key (ID)
+    const author = await Author.findByPk(req.params.id);
+    // If an author is found, send it as a JSON response
+    if (author) {
+      res.json(author);
+    } else {
+      // If no author is found, send a 404 status code and a message
+      res.status(404).send("Author not found");
+    }
+  } catch (error) {
+    // If there is an error, send the error as a JSON response
+    res.json({ error: error.message });
   }
 }
 
