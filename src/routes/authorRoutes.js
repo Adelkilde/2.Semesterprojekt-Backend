3d5c25f5111c6ed1af8be373f4e7a9ae896fe07e
Oliver Kirk <owk0907@gmail.com>
2023-11-28 14:54:21 +0100

    Try and catch med kun 1 fejlbesked i author

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -3,47 +3,49 @@ import Author from "../models/authorModel.js";
 
 // Function to handle requests to get all authors
 async function getAllAuthors(req, res) {
-  // Use the findAll method on the Author model to get all authors from the database
-  const authors = await Author.findAll();
-  // Send the authors as a JSON response
-  res.json(authors);
+  try {
+    const authors = await Author.findAll();
+    res.json(authors);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.error('Error in getAllAuthors:', error);
+  }
 }
 
 // Function to handle requests to get an author by ID
 async function getAuthorById(req, res) {
-  // Use the findByPk method on the Author model to get an author by its primary key (ID)
-  const author = await Author.findByPk(req.params.id);
-  if (author) {
-    // If the author is found, send it as a JSON response
+  try {
+    const author = await Author.findByPk(req.params.id);
     res.json(author);
-  } else {
-    // If the author is not found, send a 404 status code and a message
-    res.status(404).send("Author not found");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.error('Error in getAuthorById:', error);
   }
 }
 
 // Function to handle requests to add a new author
 async function addNewAuthor(req, res) {
-  // Use the create method on the Author model to create a new author with the data in the request body
-  const newAuthor = await Author.create(req.body);
-  // Send the new author as a JSON response
-  res.json(newAuthor);
+  try {
+    const newAuthor = await Author.create(req.body);
+    res.json(newAuthor);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.error('Error in addNewAuthor:', error);
+  }
 }
 
 // Function to handle requests to update an author
 async function updateAuthor(req, res) {
-  // Use the findByPk method on the Author model to get an author by its primary key (ID)
-  const author = await Author.findByPk(req.params.id);
-  if (author) {
-    // If the author is found, update it with the data in the request body
+  try {
+    const author = await Author.findByPk(req.params.id);
     await author.update(req.body);
-    // Send the updated author as a JSON response
     res.json(author);
-  } else {
-    // If the author is not found, send a 404 status code and a message
-    res.status(404).send("Author not found");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.error('Error in updateAuthor:', error);
   }
 }
+
 
 // Export the route handler functions
 export { getAllAuthors, getAuthorById, addNewAuthor, updateAuthor };
