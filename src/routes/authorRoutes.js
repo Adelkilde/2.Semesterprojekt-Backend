d0cabc04873824bb2f6569439af5dc9c4cbc4b26
Hjalte Albergson <123952556+HAlbergson@users.noreply.github.com>
2023-11-29 11:38:58 +0100

    feaasd

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -3,45 +3,80 @@ import Author from "../models/authorModel.js";
 
 // Function to handle requests to get all authors
 async function getAllAuthors(req, res) {
-  // Use the findAll method on the Author model to get all authors from the database
-  const authors = await Author.findAll();
-  // Send the authors as a JSON response
-  res.json(authors);
+  try {
+    // Use the findAll method on the Author model to get all authors from the database
+    const authors = await Author.findAll();
+    // Send the authors as a JSON response
+    res.json(authors);
+  } catch (error) {
+    // If an error occurs, handle it here
+    console.error("Error in getAllAuthors:", error);
+    // Send an appropriate error response to the client
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
 // Function to handle requests to get an author by ID
 async function getAuthorById(req, res) {
-  // Use the findByPk method on the Author model to get an author by its primary key (ID)
-  const author = await Author.findByPk(req.params.id);
-  if (author) {
+  try {
+    // Use the findByPk method on the Author model to get an author by its primary key (ID)
+    const author = await Author.findByPk(req.params.id);
+
+    if (!author) {
+      // If the author is not found, send a 404 status code and a message
+      res.status(404).send("Author not found");
+      console.log("Author not found");
+    }
+
     // If the author is found, send it as a JSON response
     res.json(author);
-  } else {
-    // If the author is not found, send a 404 status code and a message
-    res.status(404).send("Author not found");
+  } catch (error) {
+    // If an error occurs, handle it here
+    console.error("Error in getAuthorById:", error);
+
+    // Send an appropriate error response to the client for other types of errors
+    res.status(500).json({ error: "Internal Server Error" });
   }
 }
 
 // Function to handle requests to add a new author
 async function addNewAuthor(req, res) {
-  // Use the create method on the Author model to create a new author with the data in the request body
-  const newAuthor = await Author.create(req.body);
-  // Send the new author as a JSON response
-  res.json(newAuthor);
+  try {
+    // Use the create method on the Author model to create a new author with the data in the request body
+    const newAuthor = await Author.create(req.body);
+    // Send the new author as a JSON response
+    res.json(newAuthor);
+  } catch (error) {
+    // If an error occurs, handle it here
+    console.error("Error in addNewAuthor:", error);
+
+    // Your existing if-statements can go here
+    // For example, checking if the error is due to validation failures
+    if (error.name === "SequelizeValidationError") {
+      res.status(400).json({ error: "Validation Error", details: error.errors });
+    } else {
+      // Send an appropriate error response to the client for other types of errors
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
 }
 
-// Function to handle requests to update an author
 async function updateAuthor(req, res) {
-  // Use the findByPk method on the Author model to get an author by its primary key (ID)
-  const author = await Author.findByPk(req.params.id);
-  if (author) {
+  try {
+    // Use the findByPk method on the Author model to get an author by its primary key (ID)
+    const author = await Author.findByPk(req.params.id);
     // If the author is found, update it with the data in the request body
     await author.update(req.body);
     // Send the updated author as a JSON response
     res.json(author);
-  } else {
+  } catch (error) {
     // If the author is not found, send a 404 status code and a message
-    res.status(404).send("Author not found");
+    if (error.name === "SequelizeEmptyResultError") {
+      res.status(404).send("Author not found");
+    } else {
+      // If there's an error, send a 500 status code and the error message
+      res.status(500).send(error.message);
+    }
   }
 }
 
--- src/routes/contactMeRoutes.js
+++ src/routes/contactMeRoutes.js
@@ -3,47 +3,83 @@ import ContactMe from "../models/contactMeModel.js";
 
 // Function to handle requests to get all contact messages
 async function getAllContactMessages(req, res) {
-  // Use the findAll method on the ContactMe model to get all contact messages from the database
-  const contactMessages = await ContactMe.findAll();
-  // Send the contact messages as a JSON response
-  res.json(contactMessages);
-}
-
-// Function to handle requests to get a contact message by ID
-async function getContactMessageById(req, res) {
-  // Use the findByPk method on the ContactMe model to get a contact message by its primary key (ID)
-  const contactMessage = await ContactMe.findByPk(req.params.id);
-  if (contactMessage) {
-    // If the contact message is found, send it as a JSON response
-    res.json(contactMessage);
-  } else {
-    // If the contact message is not found, send a 404 status code and a message
-    res.status(404).send("Contact message not found");
+  try {
+    // Use the findAll method on the ContactMe model to get all contact messages from the database
+    const contactMessages = await ContactMe.findAll();
+    // Send the contact messages as a JSON response
+    res.json(contactMessages);
+  } catch (error) {
+    // If an error occurs, handle it here
+    console.error("Error in getAllContactMessages:", error);
+    // Send an appropriate error response to the client
+    res.status(500).json({ error: "Internal Server Error" });
   }
 }
 
+
+// Function to handle requests to get a contact message by ID
+async function getContactMessageById(req, res) {
+  try {
+    // Use the findByPk method on the ContactMe model to get a contact message by its primary key (ID)
+    const contactMessage = await ContactMe.findByPk(req.params.id);
+
+    // If the contact message is found, send it as a JSON response
+    res.json(contactMessage);
+  } catch (error) {
+    // If an error occurs, handle it here
+    console.error("Error in getContactMessageById:", error);
+
+    // If the error is due to the contact message not being found, send a 404 status code and a message
+    if (error.name === "SequelizeEmptyResultError") {
+      res.status(404).send("Contact message not found");
+    } else {
+      // Send an appropriate error response to the client for other types of errors
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+}
+
+
 // Function to handle requests to add a new contact message
 async function addNewContactMessage(req, res) {
-  // Use the create method on the ContactMe model to create a new contact message with the data in the request body
-  const newContactMessage = await ContactMe.create(req.body);
-  // Send the new contact message as a JSON response
-  res.json(newContactMessage);
+  try {
+    // Use the create method on the ContactMe model to create a new contact message with the data in the request body
+    const newContactMessage = await ContactMe.create(req.body);
+    // Send the new contact message as a JSON response
+    res.json(newContactMessage);
+  } catch (error) {
+    // If an error occurs, handle it here
+    console.error("Error in addNewContactMessage:", error);
+    // Send an appropriate error response to the client
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
+
 
 // Function to handle requests to update a contact message
 async function updateContactMessage(req, res) {
-  // Use the findByPk method on the ContactMe model to get a contact message by its primary key (ID)
-  const contactMessage = await ContactMe.findByPk(req.params.id);
-  if (contactMessage) {
+  try {
+    // Use the findByPk method on the ContactMe model to get a contact message by its primary key (ID)
+    const contactMessage = await ContactMe.findByPk(req.params.id);
+
     // If the contact message is found, update it with the data in the request body
     await contactMessage.update(req.body);
     // Send the updated contact message as a JSON response
     res.json(contactMessage);
-  } else {
-    // If the contact message is not found, send a 404 status code and a message
-    res.status(404).send("Contact message not found");
+  } catch (error) {
+    // If an error occurs, handle it here
+    console.error("Error in updateContactMessage:", error);
+
+    // If the error is due to the contact message not being found, send a 404 status code and a message
+    if (error.name === "SequelizeEmptyResultError") {
+      res.status(404).send("Contact message not found");
+    } else {
+      // Send an appropriate error response to the client for other types of errors
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
 }
+
 
 // Export the route handler functions
 export { getAllContactMessages, getContactMessageById, addNewContactMessage, updateContactMessage };
